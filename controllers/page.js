7ba648d1ea6411448321eefc054d91dbf720e29a
JamilRidwan <jamilridwan28@gmail.com>
2022-10-22 13:25:37 +0700

    Route dan server

---
+++ controllers/page.js
@@ -0,0 +1,11 @@
+export const Home = (req, res) =>{
+    res.send('Selamat Datang Di Halaman Home');
+}
+
+export const About = (req,res) => {
+    res.send('Selamat Datang Di Halaman About');
+}
+
+export const Contact = (req,res) => {
+    res.send('Selamat Datang Di Halaman Contact');
+}
